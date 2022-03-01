2a7cdd34fc3a2cd64a22d02b067df7dc088c7709
Chathura Yapa <chathura.yapa@solita.fi>
2022-03-01 09:47:34 +0200

    Added loading text tot he button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,12 +6,13 @@ type ButtonVariants = Readonly<"primary" | "secondary" >;
 type Props =  Readonly<{
     displayName:string;
     variant:ButtonVariants;
+    isLoading?:boolean;
     onClickAction: ()=>Promise<void>;
 }>;
 
-const Button = ({displayName,variant,onClickAction}:Props) =>(
-    <BootstrapButton variant={variant} onClick={async ()=>await onClickAction()}>
-        {displayName}
+const Button = ({displayName,variant,isLoading,onClickAction}:Props) =>(
+    <BootstrapButton disabled={isLoading} variant={variant} onClick={async ()=>await onClickAction()}>
+        {isLoading?'...':displayName}
     </BootstrapButton>
 );
 

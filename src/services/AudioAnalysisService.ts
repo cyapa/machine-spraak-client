f1a775786e753dda0f577e4e100e3c13964ffff7
Chathura Yapa <chathura.yapa@solita.fi>
2022-03-01 09:47:20 +0200

    Added Service layer with API calls

---
+++ src/services/AudioAnalysisService.ts
@@ -0,0 +1,61 @@
+import Amplify, { API } from "aws-amplify";
+
+const RESOURCES = {
+
+  HelloWorldResource:
+    {
+      name: "HelloWorldAPI",
+      endpoint: "http://127.0.0.1:3000",
+    },
+    AudioAnalysisResource:
+    {
+      name: "machinespraak_api",
+      endpoint: process.env.REACT_APP_API_URL,
+    }
+};
+
+Amplify.configure({
+    API: {
+      endpoints: [
+        RESOURCES.AudioAnalysisResource,
+        RESOURCES.HelloWorldResource
+      ],
+    },
+  });
+
+
+async function getHelloWorldContent():Promise<string>{
+    try{
+      const init = {
+        response: true,
+      };
+        const result =  await API.get(RESOURCES.HelloWorldResource.name, "/hello_world_example",init);
+        return result;
+    }
+    catch{
+        return "SERVER_ERROR";
+    }
+};
+
+async function postFile(file:any):Promise<string>{
+  try{
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("file_name", file.name);
+    const content = {
+      body: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    return await API.post(RESOURCES.AudioAnalysisResource.name, "/audio_analysis",content);
+
+  }
+  catch{
+      return "SERVER_ERROR";
+  }
+};
+
+
+export  {getHelloWorldContent,postFile};
